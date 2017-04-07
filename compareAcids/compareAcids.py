dfbf022d3e1ff45365c110cacd7b9b26fbd8316b
MayaPetek <mayapetek1@gmail.com>
2017-04-07 11:40:30 +0100

    remove duplicated stringutils

--- compareAcids/compareAcids.py
+++ compareAcids/compareAcids.py
@@ -71,26 +71,6 @@ def cullEnds(readMatch):
 
     return readMatch;
 
-
-def getChar(target, index):
-    """
-    Get a char from a string, or "-" if `index` is out of range.
-    """
-    if index < 0 or index >= len(target):
-        return "-";
-
-    return target[index];
-
-def getChars(target, index, n):
-    """
-    Get a sequence of `n` chars from `target` at `index`, safely, via `getChar`
-    """
-    out = ""
-    for i in range(0, n):
-        out += getChar(target, index + i);
-
-    return out;
-
 def getStartPoint(readMatch, refMatch):
     """
     Figure out where the first symbol of interest in the read is. This start point should be:
