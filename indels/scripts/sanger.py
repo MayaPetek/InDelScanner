9446789bea1029157018a6c1a68de2d6abb967ea
MayaPetek <mayapetek1@gmail.com>
2018-10-16 12:43:23 +0100

    ab1-fastq conversion fixed. edited trim_fastq_biopython to process one sequence only

--- indels/scripts/sanger.py
+++ indels/scripts/sanger.py
@@ -13,34 +13,19 @@ import csv
 
 import Bio
 from Bio import SeqIO, AlignIO
-from Bio.Seq import MutableSeq
 from Bio.Alphabet import IUPAC
 from Bio.Emboss.Applications import NeedleallCommandline
 
 import numpy as np
 
 
-def trim_fastq_biopython(in_seq, out_file, q_cutoff=50, consec=5, id=None, rc=False):
+def trim_fastq_biopython(in_seq, out_file, rc, q_cutoff=50, consec=5):
     """
-    Trim a FASTQ file and write out the trimmed sequences as a FASTQ file.
-
-    Only processes the sequence with identifer string rec.  If id
-    is None, takes first sequence.
+    Trim one FASTQ file and write out the trimmed sequences as a FASTQ file.
     """
-    # Load in sequences using Bio.SeqIO.  We'll keep the result as a dict.
-    seqs = Bio.SeqIO.to_dict(in_seq)
-
-    # Pull out the id we want
-    if id is None:
-        key, seq = seqs.popitem()
-    else:
-        try:
-            seq = seqs[id]
-        except KeyError:
-            raise KeyError('id not found in input file')
 
     # Get Boolean array for good quality
-    q_good = np.array(seq.letter_annotations['phred_quality']) >= q_cutoff
+    q_good = np.array(in_seq.letter_annotations['phred_quality']) >= q_cutoff
 
     # Find first set of consec good bases
     i = 0
@@ -56,36 +41,19 @@ def trim_fastq_biopython(in_seq, out_file, q_cutoff=50, consec=5, id=None, rc=Fa
     with open(out_file, 'w') as f:
         if rc:
             # if the sequence is from a reverse primer, reverse-complement first
-            rc = seq[i:j].reverse_complement(id=True, name=True, description=True)
-            Bio.SeqIO.write(rc, f, 'fastq')
+            rev_seq = in_seq[i:j].reverse_complement(id=True, name=True, description=True)
+            Bio.SeqIO.write(rev_seq, f, 'fastq')
         else:
-            Bio.SeqIO.write(seq[i:j], f, 'fastq')
+            Bio.SeqIO.write(in_seq[i:j], f, 'fastq')
 
 
-def ab1_to_fastq(fname, rc=False, q_cutoff=50):
+def convert_ab1(ab1_dir, rc):
     """
-    Convert one ab1 file to trimmed fastq
-    :param fname:
-    :param rc:
-    :param q_cutoff:
-    :return:
-    """
-
-    # Get the prefix of the file
-    prefix, suffix = os.path.splitext(fname)
-
-    # Convert and trim FASTQ file
-    Bio.SeqIO.convert(fname, 'abi', prefix + '.fastq', 'fastq')
-    trim_fastq_biopython(prefix + '.fastq', prefix + '_trimmed.fastq', rc=rc, q_cutoff=q_cutoff)
-
-    return prefix + '_trimmed.fastq'
-
-
-def convert_ab1(ab1_dir, rc=False):
-    """
+    Wrapper function.
     Convert all *.ab1 files in input directory to trimmed fastq sequences.
     Write result to new *.fq files in 'fastq' directory.
     :param ab1_dir: Path to directory containing ab1 files.
+    :param rc: True/False - reverse complement the sequence
     :return:
     """
 
@@ -101,10 +69,9 @@ def convert_ab1(ab1_dir, rc=False):
         fastq_fname = prefix + '.fastq'
 
         # Use Biopython to convert file format, then trim the results
-        record = SeqIO.read(fname, 'abi')
         Bio.SeqIO.convert(fname, 'abi', fastq_fname, 'fastq')
-
-        trim_fastq_biopython(fname, prefix + '.fq', rc)
+        record = SeqIO.read(fastq_fname, 'fastq')
+        trim_fastq_biopython(record, prefix + '.fq', rc)
 
         # Remove the temporary full fastq file
         os.remove(fastq_fname)
@@ -158,11 +125,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--reference', help='FASTA file with reference sequence', required=True)
     parser.add_argument('-d', '--directory', help='Directory containing ab1 files', required=True)
-    parser.add_argument('--input', help='CSV file documenting samples', required=True)
+    parser.add_argument('--input', help='CSV file documenting samples, encoded with Unicode-8', required=True)
     parser.add_argument('-o', '--output', help='Name of output CSV file', required=True)
     args = parser.parse_args()
 
-    convert_ab1(args.directory, rc=True)
+    # convert_ab1(args.directory, rc=True)
 
     with open(args.output, 'w') as out:
         fieldnames = ['Sample', 'DNA_hgvs', 'Protein_short', 'Protein_tuple']
